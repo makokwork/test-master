7c8c4327480eee7d2efde0f84a79bb5ada48dc26
Vitaliy <frdopoesd@yandex.ru>
2024-05-11 01:21:36 +0300

    test

--- src/pages/AdminPage/Admin.jsx
+++ src/pages/AdminPage/Admin.jsx
@@ -4,7 +4,8 @@ import Button from '@mui/material/Button';
 import './admin.scss';
 import 'easymde/dist/easymde.min.css';
 import { useAuth } from '../../hooks/useAuth';
-import BlogCreation from './components/BlogCreation'
+import BlogCreation from './components/BlogCreation/BlogCreation';
+import ProjectsCreation from './components/ProjectsCreation/ProjectsCreation';
 
 const Admin = () => {
   const { logout } = useAuth();
@@ -23,27 +24,31 @@ const Admin = () => {
             <h3>Управление контентом</h3>
             <div className="nav-menu__content">
               <ul>
-                <NavLink to="/admin">
+                <NavLink to="/projects">
                   <li>Проекты</li>
                 </NavLink>
 
-                <NavLink to="/admin">
+                <NavLink to="reports">
                   <li>Отчеты</li>
                 </NavLink>
 
-                <NavLink to="/admin">
+                <NavLink to="documents">
                   <li>Документы</li>
                 </NavLink>
 
-                <NavLink to="/admin">
+                <NavLink to="help-others">
                   <li>Помочь другим</li>
                 </NavLink>
 
-                <NavLink to="/admin">
+                <NavLink to="get-help">
+                  <li>Получить помощь</li>
+                </NavLink>
+
+                <NavLink to="blog">
                   <li>Блог</li>
                 </NavLink>
               </ul>
-              <NavLink to="/admin">
+              <NavLink to="appeals">
                 <p>Обращения</p>
               </NavLink>
             </div>
@@ -56,6 +61,7 @@ const Admin = () => {
         <div className="main__content">
           <Routes>
             <Route path="blog" element={<BlogCreation />} />
+            <Route path="projects" element={<ProjectsCreation />} />
           </Routes>
         </div>
       </main>
