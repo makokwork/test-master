4ac6c7a3649e31ea59bca8d56b20828af1ea8480
sol0vey <solovevdaniil03@mail.ru>
2024-05-21 23:05:15 +0300

    fix hz cho

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -6,11 +6,11 @@ import Header from '../../components/Header/Header';
 import Videoholder from './components/Videoholder/Videoholder';
 import SocialActivity from './components/SocialActivity/SocialActivity';
 import Trust from './components/Trust/Trust';
-import Charity from '../../components/CharitySlider/Charity';
 import Documents from './components/Documents/Documents';
 import Banner from './components/Banner/Banner';
 import Partners from '../../components/CharitySlider/Partners';
 import Hero from '../../components/Hero/Hero';
+import Slider from '../../components/CharitySlider/Swiper/Swiper';
 
 function Company() {
   return (
@@ -20,7 +20,7 @@ function Company() {
       <HeroNav />
       <Videoholder />
       <SocialActivity />
-      <Charity />
+      <Slider />
       <Partners />
       <Trust />
       <Documents />
