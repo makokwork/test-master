dab178de212762658d40cde28d0fc086921ed05b
Vitaliy <frdopoesd@yandex.ru>
2024-05-21 22:12:07 +0300

    123

--- src/components/CharitySlider/CharitySlider.jsx
+++ src/components/CharitySlider/CharitySlider.jsx
@@ -1,40 +1,11 @@
+import Slider from './Swiper/Swiper';
+
 function CharitySlider() {
   return (
     <div className="charity-photos">
       <div className="charity-photos__body">
         <div className="charity-photos__inner">
-          <figure>
-            <img
-              src="./images/section-charity-1.jpg"
-              width="350"
-              height="500"
-              alt="Люди"
-              loading="lazy"
-            />
-            <figcaption>Открытие благотворительной ярмарки в городе Город</figcaption>
-          </figure>
-
-          <figure>
-            <img
-              src="./images/section-charity-2.jpg"
-              width="350"
-              height="500"
-              alt="Люди"
-              loading="lazy"
-            />
-            <figcaption>Сбор гуманитарной помощи</figcaption>
-          </figure>
-
-          <figure>
-            <img
-              src="./images/section-charity-3.jpg"
-              width="350"
-              height="500"
-              alt="Люди"
-              loading="lazy"
-            />
-            <figcaption>Выступление на важных общественных мероприятиях</figcaption>
-          </figure>
+          <Slider />
         </div>
       </div>
     </div>
--- src/components/CharitySlider/Swiper/Swiper.jsx
+++ src/components/CharitySlider/Swiper/Swiper.jsx
@@ -6,44 +6,41 @@ import 'swiper/css/pagination';
 
 function Slider() {
   return (
-    <section className="section container-about">
-      <Swiper
-        slidesPerView={3}
-        slidesPerGroup={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-        loop
-      >
-        <SwiperSlide>
-          <img src="images/slide/slide1.png" alt="slide1" />
-          <p className='swiper__slide__desc'>Открытие благотворительной ярмарки в городе Город</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/slide/slide2.png" alt="slide2" />
-          <p className='swiper__slide__desc'>Сбор гуманитарной помощи участникам СВО</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/slide/slide3.png" alt="slide3" />
-          <p className='swiper__slide__desc'>Выступление на важных общественных мероприятиях</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/slide/slide4.png" alt="slide4" />
-          <p className='swiper__slide__desc'>Крестный ход в Ростове-на-Дону</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/slide/slide5.png" alt="slide5" />
-          <p className='swiper__slide__desc'>Сбор гуманитарной помощи участникам СВО</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="images/slide/slide6.png" alt="slide6" />
-          <p className='swiper__slide__desc'>Выступление на важных общественных мероприятиях</p>
-        </SwiperSlide>
-      </Swiper>
-    </section>
+    <Swiper
+      slidesPerView={3}
+      slidesPerGroup={3}
+      spaceBetween={30}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="mySwiper"
+      loop>
+      <SwiperSlide>
+        <img src="images/slide/slide1.png" alt="slide1" />
+        <p className="swiper__slide__desc">Открытие благотворительной ярмарки в городе Город</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="images/slide/slide2.png" alt="slide2" />
+        <p className="swiper__slide__desc">Сбор гуманитарной помощи участникам СВО</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="images/slide/slide3.png" alt="slide3" />
+        <p className="swiper__slide__desc">Выступление на важных общественных мероприятиях</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="images/slide/slide4.png" alt="slide4" />
+        <p className="swiper__slide__desc">Крестный ход в Ростове-на-Дону</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="images/slide/slide5.png" alt="slide5" />
+        <p className="swiper__slide__desc">Сбор гуманитарной помощи участникам СВО</p>
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src="images/slide/slide6.png" alt="slide6" />
+        <p className="swiper__slide__desc">Выступление на важных общественных мероприятиях</p>
+      </SwiperSlide>
+    </Swiper>
   );
 }
 
