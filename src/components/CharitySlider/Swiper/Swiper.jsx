2ca277c78198aa96c931db7b066cb1f9cf2ad769
Vitaliy <frdopoesd@yandex.ru>
2024-05-21 23:19:13 +0300

    2

--- src/components/CharitySlider/Swiper/Swiper.jsx
+++ src/components/CharitySlider/Swiper/Swiper.jsx
@@ -7,6 +7,19 @@ import 'swiper/css/pagination';
 function Slider() {
   return (
     <section className="section container-about">
+      <header className="section__header">
+        <h2 className="section__title" id="fourth-section">
+          Больше чем благотворительность
+        </h2>
+        <div className="section__description">
+          <p>
+            Мы сформировали собственное видение о помощи людям и гордимся нашей деятельностью
+            <br />
+            <br />
+            Стремление к новым вершинам - наш ключ к успеху
+          </p>
+        </div>
+      </header>
       <Swiper
         spaceBetween={30}
         pagination={{
@@ -16,39 +29,38 @@ function Slider() {
         breakpoints={{
           1160: {
             slidesPerView: 3,
-            slidesPerGroup: 3
+            slidesPerGroup: 3,
           },
           790: {
             slidesPerView: 2,
-            slidesPerGroup: 2
+            slidesPerGroup: 2,
           },
         }}
         className="mySwiper"
-        loop
-      >
+        loop>
         <SwiperSlide>
           <img src="images/slide/slide1.png" alt="slide1" />
-          <p className='swiper__slide__desc'>Открытие благотворительной ярмарки в городе Город</p>
+          <p className="swiper__slide__desc">Открытие благотворительной ярмарки в городе Город</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src="images/slide/slide2.png" alt="slide2" />
-          <p className='swiper__slide__desc'>Сбор гуманитарной помощи участникам СВО</p>
+          <p className="swiper__slide__desc">Сбор гуманитарной помощи участникам СВО</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src="images/slide/slide3.png" alt="slide3" />
-          <p className='swiper__slide__desc'>Выступление на важных общественных мероприятиях</p>
+          <p className="swiper__slide__desc">Выступление на важных общественных мероприятиях</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src="images/slide/slide4.png" alt="slide4" />
-          <p className='swiper__slide__desc'>Крестный ход в Ростове-на-Дону</p>
+          <p className="swiper__slide__desc">Крестный ход в Ростове-на-Дону</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src="images/slide/slide5.png" alt="slide5" />
-          <p className='swiper__slide__desc'>Сбор гуманитарной помощи участникам СВО</p>
+          <p className="swiper__slide__desc">Сбор гуманитарной помощи участникам СВО</p>
         </SwiperSlide>
         <SwiperSlide>
           <img src="images/slide/slide6.png" alt="slide6" />
-          <p className='swiper__slide__desc'>Выступление на важных общественных мероприятиях</p>
+          <p className="swiper__slide__desc">Выступление на важных общественных мероприятиях</p>
         </SwiperSlide>
       </Swiper>
     </section>
--- src/pages/PolicyPage/Policy.jsx
+++ src/pages/PolicyPage/Policy.jsx
@@ -1,13 +1,12 @@
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import PolicyContainer from './components/PolicyContainer';
-import Slider from '../../components/CharitySlider/Swiper/Swiper';
 function Policy() {
   return (
     <>
       <Header />
-      <Slider />
-      {/* <PolicyContainer /> */}
+
+      <PolicyContainer />
       <Footer />
     </>
   );
